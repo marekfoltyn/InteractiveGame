046bbdea7c794daec72bdbb69b1a77b17dd96305
Marek Foltýn <marek@foltynovi.cz>
2015-12-30 16:26:16 +0100

    added TestScene, TestDelegate, Catch moved to Game

---
+++ source/game/InteractiveGame/Classes/TestScene.cpp
@@ -0,0 +1,42 @@
+#include "TestScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+
+#include "MessageIdentifiers.h"
+#include "RakPeerInterface.h"
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+
+Scene * TestScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = TestScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool TestScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    auto rootNode = CSLoader::createNode("MainScene.csb");
+
+    addChild(rootNode);
+
+    return true;
+}
