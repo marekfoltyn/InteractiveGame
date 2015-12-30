986f62c6f2ad37562b2d3d2650661c5510de516e
Marek Foltýn <marek@foltynovi.cz>
2015-12-30 16:34:52 +0100

    added NetworkFramework project

---
+++ source/network/NetworkFramework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NetworkFramework
+//
+//  Created by Marek Foltýn on 30.12.15.
+//  Copyright © 2015 foltyma1. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
